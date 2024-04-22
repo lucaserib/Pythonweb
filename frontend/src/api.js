9c9349a540b4515aeed1110809c01dd27d637c0b
Lucas <lucasemanuelribeiro@hotmail.com>
2024-04-22 13:24:43 -0300

    terminando validação de tokens

---
+++ frontend/src/api.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { ACCESS_TOKEN } from "./constants"
+
+const api = axios.create({
+    base_URL: import.meta.env.VITE_API_URL
+})
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStoage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Autorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) =>{
+        return Promise.reject(error)
+    }
+)
+
+
+export default api
